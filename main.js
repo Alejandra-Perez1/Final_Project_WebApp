8e3ceac7b7f45342d651618dd4373f352ce59937
GomaNiroula <goma.niroula@ucdenver.edu>
2021-03-24 20:13:16 -0600

    Added more depndencies and also added half the code for mongoose

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 "use strict";
+const express = require("express"), app = express(),
+homeController = require("./controllers/homeController"),
+errorController = require("./controllers/errorController"),
+usersController = require("./controllers/usersController"),
+layouts = require("express-ejs-layouts");
+
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://localhost:27017/confetti_cuisine",
+  { useNewUrlParser: true }
+);
+mongoose.set("useCreateIndex", true);
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+app.use(layouts);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//Add all the controllers here
+
+app.listen(app.get("port"), () => {
+    console.log(`Server running at http://localhost:${app.get("port")}`);
+  });
---
+++ models/user.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const mongoose = require("mongoose"),
+{ Schema } = require("mongoose");
+
+var userSchema = new Schema(
+  {
+    name: {
+      first: {
+        type: String,
+        trim: true
+      },
+      last: {
+        type: String,
+        trim: true
+      }
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    location: {
+        type: String,
+        trim: true
+    },
+    securityquestion1: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    securityquestion2: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    securityquestion3: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    biography: {
+        type: String, 
+        trim: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+userSchema.virtual("fullName").get(function() {
+  return `${this.name.first} ${this.name.last}`;
+});
+
+module.exports = mongoose.model("User", userSchema);

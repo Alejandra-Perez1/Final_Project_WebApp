6f06b3d7ae9949a7b2feb3a14d77a6af8df4aa16
GomaNiroula <goma.niroula@ucdenver.edu>
2021-03-24 20:30:39 -0600

    Updated usersController and main file with DB

--- main.js
+++ main.js
@@ -1,33 +1,55 @@
 "use strict";
-const express = require("express"), app = express(),
-homeController = require("./controllers/homeController"),
-errorController = require("./controllers/errorController"),
-usersController = require("./controllers/usersController"),
-layouts = require("express-ejs-layouts");
 
-const mongoose = require("mongoose");
+const express = require("express"),
+  layouts = require("express-ejs-layouts"),
+  app = express(),
+  router = express.Router(),
+  homeController = require("./controllers/homeController"),
+  errorController = require("./controllers/errorController"),
+  usersController = require("./controllers/usersController.js"),
+  mongoose = require("mongoose"),
+  methodOverride = require("method-override");
+
 mongoose.connect(
-  "mongodb://localhost:27017/confetti_cuisine",
+  "mongodb://localhost:27017/chitter_chatter",
   { useNewUrlParser: true }
 );
 mongoose.set("useCreateIndex", true);
-app.set("view engine", "ejs");
+
 app.set("port", process.env.PORT || 3000);
-app.use(
+app.set("view engine", "ejs");
+
+router.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"]
+  })
+);
+
+router.use(layouts);
+router.use(express.static("public"));
+
+router.use(
   express.urlencoded({
     extended: false
   })
 );
-app.use(express.json());
-app.use(layouts);
-app.use(express.static("public"));
+router.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+router.get("/", homeController.index);
 
-//Add all the controllers here
+router.get("/users", usersController.index, usersController.indexView);
+router.get("/users/new", usersController.new);
+router.post("/users/create", usersController.create, usersController.redirectView);
+router.get("/users/:id/edit", usersController.edit);
+router.put("/users/:id/update", usersController.update, usersController.redirectView);
+router.get("/users/:id", usersController.show, usersController.showView);
+router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
+
+router.use(errorController.pageNotFoundError);
+router.use(errorController.internalServerError);
+
+app.use("/", router);
 
 app.listen(app.get("port"), () => {
-    console.log(`Server running at http://localhost:${app.get("port")}`);
-  });
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});

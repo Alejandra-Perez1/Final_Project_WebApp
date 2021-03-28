c426bc42b7ed60fdb7fe4c2af17ff2218ef1cf88
Sharvita Paithankar <sharvitap123@gmail.com>
2021-03-27 22:25:51 -0600

    Update homeController.js

---
+++ controllers/homeController.js
@@ -0,0 +1,11 @@
+exports.showSignUp = ( req, res) => {
+    res.render("signup")
+}
+
+exports.showIndex = (req,res) => {
+    res.render("index");
+
+}
+exports.showSignIn = ( req, res) => {
+    res.render("signin")
+}
